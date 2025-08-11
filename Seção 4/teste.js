8c9bd805f89c34221bee7b424a2caef4caa51781
Pedro Lyrio <pedro.lyrio100@gmail.com>
2025-08-11 15:38:43 -0300

    Seção 4

---
+++ Seção 4/teste.js
@@ -0,0 +1,18 @@
+// algoritmo básico
+
+console.log("Sentença de código");
+
+// Bloco de código
+
+{
+    console.log("Bloco de código");
+    console.log("Outra sentença de código");
+}
+
+// Variáveis e constantes
+let variavel = "Variável";
+const constante = "Constante";
+var variavelAntiga = "Variável antiga";
+console.log(variavel);
+console.log(constante);
+console.log(variavelAntiga);
